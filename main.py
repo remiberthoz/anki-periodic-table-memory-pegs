2695ce556aedfe2966063442776dc3541b81b1e7
Rémi Berthoz <remi.berthoz@gmail.com>
2025-11-24 20:59:10 +0100

    Revert "Indicate version number in package name" (sync issue)
    Version number cannot be in deck name, must be in the description
    as originally suggested.
    Otherwise cannot be shared on AnkiWeb.
    This reverts commit 9c2be0291c2d6d68fa97ff87e739d296b83cad55.

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import subprocess
 import sys
 from markdown2 import Markdown
 
-version = subprocess.check_output(['git', 'describe', '--tags', 'HEAD']).decode('ascii').strip()
-
 mk = Markdown()
 
 css = None
@@ -32,7 +30,7 @@ with open('src/templates/stylesheet.css') as CSS, \
 
 deck = genanki.Deck(
         deck_id=490209917,
-        name=f'Periodic table memory pegs ({version})')
+        name='Periodic table memory pegs')
 
 model = genanki.Model(
         model_id=1484938894779,
